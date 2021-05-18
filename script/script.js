f6ae953a0e215c5a5183440602be52da0d2a3c9e
PaulDenis <d.paul@outlook.it>
2021-05-18 18:14:14 +0200

    Esercizio Base completo

--- script/script.js
+++ script/script.js
@@ -2,7 +2,54 @@ var app = new Vue(
     {
         el:"#root",
         data: {
+            images: [
+                {
+                    image: "img/colosseo.webp",
+                    name: "Colosseo"
+                },
+                {
+                    image: "img/Chichén_Itzá.jpg",
+                    name: "Chichén Itzá"
+                },
+                {
+                    image: "img/muraglia_cinese.jpg",
+                    name: "Grande muraglia Cinese"
+                },
+                {
+                    image: "img/taj_mahal.jpeg",
+                    name: "Taj mahal"
+                },
+                {
+                    image: "img/monasterio-petra.webp",
+                    name: "Monasterio Petra"
+                },
+                {
+                    image: "img/Machu_Picchu.jpg",
+                    name: "Machu Picchu"
+                },
+                {
+                    image: "img/Cristo_redentore.jpg",
+                    name: "Cristo Redentor"
+                } 
+            ],
+            imageIndex: 0
+        },
+        methods: {
+            prevImg: function() {
 
+                this.imageIndex = this.imageIndex - 1;
+                
+                if (this.imageIndex < 0) {
+                    this.imageIndex = this.images.length;
+                }
+            },
+            goForward: function() {
+                this.imageIndex++;
+                if(this.imageIndex == this.images.length) {
+                    this.imageIndex = 0;
+                }
+            }
         }
+        
     }
 )
